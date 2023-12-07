e4c3b0fe85808e46ee67dd011ee1f92cc9094e90
Rehammawed <alre22mq@student.ju.se>
2023-12-07 05:51:06 +0100

    retouch

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,12 @@
-let image = document.querySelector(".image");
 
-image.addEventListener("click", function () {
-  image.src = "icons/heart icon fullpink.svg";
+let heartButtons = document.querySelectorAll(".heart");
+let heartIcons = document.querySelectorAll(".heart-icon");
+
+//I have read about "forEach" here https://www.freecodecamp.org/news/javascript-foreach-js-array-for-each-example///
+
+heartButtons.forEach(function (heartButton, index) {
+  heartButton.addEventListener("click", function () {
+    heartIcons[index].classList.toggle("heart-icon");
+    heartIcons[index].classList.toggle("heart-icon-fill");
+  });
 });
-
-function changeImage(image) {
-  image.src = "icons/heart icon fullpink.svg";
-}
