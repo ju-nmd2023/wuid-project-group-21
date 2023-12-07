3dcaf5751b38b84b8447b089d53f0bd136e7d4f2
Zhiyin Li <lizh21nv@student.ju.se>
2023-12-07 12:32:40 +0100

    deleted the js out of the folder and change the var to const in the new js

--- detailpage.js
+++
@@ -1,5 +0,0 @@
-function changeImage(imageName) {  
-    const imageContainer = document.getElementById('detailmainpicture');  
-    const currentImage = imageContainer.getElementsByTagName('img')[0];  
-    currentImage.src = 'pictures/' + imageName + '.png';  
-  }  
