32f7325c49b6b09ca0da412b8bfd5294b4bc7550
liudmyla-petrus <peli23cf@student.ju.se>
2023-12-08 15:30:48 +0100

    Renamed js file

---
+++ js/categoryMenu.js
@@ -0,0 +1,13 @@
+/*const scrollingWheel = document.querySelector(".Categories");
+//Information was partly taken from: https://www.youtube.com/watch?v=0uskTP_iHNI//
+scrollingWheel.addEventListener(
+  "wheel",
+  function (e) {
+    if (e.wheelDelta > 0) {
+      this.scrollLeft -= 50;
+    } else {
+      this.scrollLeft += 50;
+    }
+  },
+  { passive: true }
+);*/
